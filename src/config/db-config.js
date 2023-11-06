a08289f110cee5e822f08626bf223668ec606cb2
UncleSubak <kko6369@naver.com>
2023-11-06 17:34:50 +0900

    Refactor: 배포 버전으로 업뎃

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -3,17 +3,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-// module.exports = {
-//   host: process.env.RDS_HOSTNAME,
-//   port: process.env.RDS_PORT,
-//   user: process.env.RDS_USERNAME,
-//   password: process.env.RDS_PASSWORD,
-//   database: process.env.RDS_DB_NAME,
-// };
 module.exports = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME,
 };
+// module.exports = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DATABASE,
+// };
