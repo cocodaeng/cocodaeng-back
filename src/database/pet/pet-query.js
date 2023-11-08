3c1d21724132c92bb422da5dfc2ac4f5a0e09425
UncleSubak <kko6369@naver.com>
2023-11-08 15:30:06 +0900

    Feat: 펫 등록 후 알러지 정보 삽입 기능 구현

--- src/database/pet/pet-query.js
+++ src/database/pet/pet-query.js
@@ -10,7 +10,17 @@ exports.findPetsByMemberNo = () => {
   `;
 };
 
-// 펫 등록
+/* 펫 번호로 펫 조회하는 메소드 - 김종완 */
+exports.findPetByPetNo = () => {
+  return `
+    SELECT *
+    FROM TBL_PETS
+    WHERE PET_pet_no = ?
+    AND PET_pet_status = 1
+  `;
+};
+
+/* 펫 등록 - 김종완 */
 exports.createPet = () => {
   return `
         INSERT INTO TBL_PETS
@@ -18,4 +28,16 @@ exports.createPet = () => {
               PET_pet_age, PET_pet_weight, PET_create_date, PET_pet_status)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     `;
+};
+
+/* 펫 등록 후 알러지 정보 삽입 - 김종완 */
+exports.createAllergies = () => {
+  return `
+    INSERT INTO TBL_PET_ALLERGY
+    ( 
+      PAG_pet_no,
+      PAG_allergy_ingredient_no
+    )
+    VALUES ?
+  `;
 };
