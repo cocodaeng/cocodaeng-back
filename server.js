8ad27479887d51694eb165c63b5e415b2c8b2726
UncleSubak <kko6369@naver.com>
2023-11-08 13:10:38 +0900

    Refactor: 설정 변경

--- server.js
+++ server.js
@@ -27,11 +27,7 @@ app.use("/api/v1/allergy", allergyRouter);
 app.use("/api/v1/diary", diaryRouter);
 app.use("/api/v1/result", resultRouter);
 
-app.listen(9000, () => console.log("Listening on port 9000!"));
-
-// app.use("/", (req, res) => {
-//   res.send("Hello, world!");
-// });
+app.listen(8080, () => console.log("Listening on port 8080!"));
 
 app.use((err, req, res, next) => {
   let statusCode = err.status || HttpStatus.INTERNAL_SERVER_ERROR; // 기본값으로 500을 사용
